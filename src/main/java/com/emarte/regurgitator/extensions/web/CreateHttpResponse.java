ac797ed50185f71d416a15e5f107c1f272f8d4bb
talmeym <mtalmey@gmail.com>
2016-01-19 15:44:52 +0000

    added create-http-response

---
+++ src/main/java/com/emarte/regurgitator/extensions/web/CreateHttpResponse.java
@@ -0,0 +1,35 @@
+package com.emarte.regurgitator.extensions.web;
+
+import com.emarte.regurgitator.core.*;
+
+import static com.emarte.regurgitator.core.CoreTypes.NUMBER;
+import static com.emarte.regurgitator.core.CoreTypes.STRING;
+import static com.emarte.regurgitator.extensions.web.ExtensionsWebConfigConstants.*;
+
+public class CreateHttpResponse extends Identifiable implements Step {
+	private final CreateResponse response;
+	private final long statusCode;
+	private final String contentType;
+
+	public CreateHttpResponse(CreateResponse response, long statusCode, String contentType) {
+		super(response.getId());
+		this.response = response;
+		this.statusCode = statusCode;
+		this.contentType = contentType;
+	}
+
+	@Override
+	public void execute(Message message) throws RegurgitatorException {
+		Parameters responseMetadata = message.getContext(RESPONSE_METADATA_CONTEXT);
+
+		if(statusCode != -1) {
+			responseMetadata.setValue(STATUS_CODE, NUMBER, statusCode);
+		}
+
+		if(contentType != null) {
+			responseMetadata.setValue(CONTENT_TYPE, STRING, contentType);
+		}
+
+		response.execute(message);
+	}
+}
